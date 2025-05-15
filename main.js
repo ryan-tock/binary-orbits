41f576690bbf359961b601ea636f55832fba64ff
Ryan Tock <ryan@tocks.org>
2025-05-15 12:38:34 -0700

    differential evolution code

--- main.js
+++ main.js
@@ -6,23 +6,34 @@ async function initializePyodide() {
 }
 
 async function loadPythonScript() {
-    const response = await fetch("myscript.py");
+    await pyodide.loadPackage("numpy");
+    await pyodide.loadPackage("scipy");
+
+    const timestamp = new Date().getTime();
+    const response = await fetch("myscript.py?cache_bust=${timestamp}");
     const pythonCode = await response.text();
     await pyodide.runPythonAsync(pythonCode);
     console.log("Python script loaded");
 }
 
 async function runPythonFunction() {
-    try {
-        const result = pyodide.runPython("test()");
-        document.getElementById("output").textContent = result;
-    } catch (error) {
-        console.error("Error executing Python function:", error);
-        document.getElementById("output").textContent = "Error: " + error;
-    }
+    const result = pyodide.runPython("optimize([{'t':1890.43,'x':0.15,'y':0.187,'weight':1,'method':1}, {'t':1893.48,'x':0.05,'y':0.235,'weight':1,'method':1}, {'t':1898.44,'x':-0.119,'y':0.276,'weight':1,'method':1}])");
+    const parameters = result.toJs();
+    console.log(parameters);
+}
+
+async function loadCalc() {
+    var elt = document.getElementById('calculator');
+    var calculator = Desmos.Calculator3D(elt);
+
+    const response = await fetch('./state.json');
+    const json = await response.json();
+    calculator.setState(JSON.stringify(json));
+    calculator.updateSettings({"expressions": false});
 }
 
 window.addEventListener("load", async () => {
     await initializePyodide();
     await loadPythonScript();
+    await loadCalc();
 });
--- myscript.py
+++ myscript.py
@@ -1,2 +1,73 @@
-def test():
-    return "Blahblah"
+import math
+import numpy as np
+from scipy.optimize import differential_evolution
+
+NEWTON_ITERATIONS = 6
+
+def optimize(data):
+    bounds = [(0,0), (0, 0.95), (0, math.pi), (0, 2 * math.pi), (0, 2 * math.pi), (0, 2 * math.pi), (1, 1000)]
+    result = differential_evolution(loss, bounds, args=(data,))
+
+    parameters = result.x.tolist()
+
+    loss(parameters, data)
+
+    return parameters
+
+def loss(parameters, data):
+    e = parameters[1]
+    i = parameters[2]
+    node = parameters[3]
+    periapsis = parameters[4]
+    m_0 = parameters[5]
+    p = parameters[6]
+
+    node_angles = [math.cos(node - 3 * math.pi / 2), math.sin(node - 3 * math.pi / 2)]
+    inclined_angle = math.cos(i)
+
+    beta = e / (1 + math.sqrt(1 - e**2))
+
+    predicted_positions = []
+    parameter_squared = 0
+    resultant = 0
+
+    for point in data:
+        t = point['t']
+        x = point['x']
+        y = point['y']
+        weight = point['weight']
+
+        mean_anomaly = m_0 + (2 * math.pi / p) * (t - 2000)
+
+        eccentric_anomaly = mean_anomaly
+
+        for _ in range(NEWTON_ITERATIONS):
+            eccentric_anomaly += (mean_anomaly - eccentric_anomaly + e * math.sin(eccentric_anomaly)) / (1 - e * math.cos(eccentric_anomaly))
+
+        true_anomaly = eccentric_anomaly + 2 * math.atan(beta * math.sin(eccentric_anomaly) / (1 - beta * math.cos(eccentric_anomaly)))
+
+        r_scaled = 1 - e * math.cos(eccentric_anomaly)
+        planar_angles = [math.cos(true_anomaly + periapsis), math.sin(true_anomaly + periapsis)]
+
+        predicted_positions.append([])
+        predicted_positions[-1].append(r_scaled * (planar_angles[0] * node_angles[0] - inclined_angle * planar_angles[1] * node_angles[1]))
+        predicted_positions[-1].append(r_scaled * (inclined_angle * planar_angles[1] * node_angles[0] + planar_angles[0] * node_angles[1]))
+
+        parameter_squared += predicted_positions[-1][0] ** 2 * weight
+        parameter_squared += predicted_positions[-1][1] ** 2 * weight
+
+        resultant += predicted_positions[-1][0] * x * weight
+        resultant += predicted_positions[-1][1] * y * weight
+    
+    sm = resultant / parameter_squared
+    if sm < 0:
+        sm = 0
+    
+    parameters[0] = sm
+
+    error = 0
+    for index in range(len(data)):
+        error += (data[index]['x'] - sm * predicted_positions[index][0]) ** 2 * data[index]['weight']
+        error += (data[index]['y'] - sm * predicted_positions[index][1]) ** 2 * data[index]['weight']
+
+    return error
