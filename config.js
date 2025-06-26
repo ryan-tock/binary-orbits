3b052be58e9fbc4cb256794445743df89bf00e41
ryan-tock <ryan-tock@users.noreply.github.com>
2025-06-26 08:40:57 +0000

    deploy: eb3971855944424895f387f9259d75ede384498b

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const DESMOS_API_KEY = "7b0f4a3a647343cebc7c908a66629ec2";
+export const DESMOS_API_KEY = "7b0f4a3a647343cebc7c908a66629ec2";\nexport const API_ENDPOINT_URL = "https://ko2hf5sz9g.execute-api.us-west-2.amazonaws.com/process";
